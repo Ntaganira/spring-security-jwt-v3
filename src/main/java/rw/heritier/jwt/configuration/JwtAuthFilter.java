6797353ab65002dc47c7b7e6de7a14c988da8be9
Ntaganira <heritier.ntaganira@aos.rw>
2023-05-17 10:32:17 +0200

    JWT claims get Signature

---
+++ src/main/java/rw/heritier/jwt/configuration/JwtAuthFilter.java
@@ -0,0 +1,45 @@
+package rw.heritier.jwt.configuration;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.micrometer.common.lang.NonNull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private static final String AUTHORIZATION = "Autorization";
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String authHeader = request.getHeader(AUTHORIZATION);
+
+        final String jwt;
+        final String userEmail;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        // Extraction token from header
+        jwt = authHeader.substring(7);
+        // Extracting user email / username
+        userEmail = jwtService.extractUsername(jwt);
+
+    }
+
+}
---
+++ src/main/java/rw/heritier/jwt/configuration/JwtService.java
@@ -0,0 +1,36 @@
+package rw.heritier.jwt.configuration;
+
+import java.security.Key;
+
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service // Transforming a class to a managed bean
+public class JwtService {
+
+    // From : https://www.allkeysgenerator.com/
+    private static final String SECRETE_KEY = "4E645267556B586E3272357538782F413F4428472B4B6250655368566D597133";
+
+    public String extractUsername(String jwt) {
+        return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+    }
+
+    Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRETE_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
