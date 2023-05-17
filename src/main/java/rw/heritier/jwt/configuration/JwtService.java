8dcb4e5b130e97d43b38a3cba9a49ddc144f9c0b
Ntaganira <heritier.ntaganira@aos.rw>
2023-05-17 11:21:51 +0200

    Is the token valid method is implemented!

--- src/main/java/rw/heritier/jwt/configuration/JwtService.java
+++ src/main/java/rw/heritier/jwt/configuration/JwtService.java
@@ -1,7 +1,7 @@
 package rw.heritier.jwt.configuration;
 
 import java.security.Key;
-import java.sql.Date;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
@@ -43,6 +43,19 @@ public class JwtService {
                 .setExpiration(new java.util.Date(System.currentTimeMillis() + 1000 * 60 * 24))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 
     private Claims extractAllClaims(String token) {
