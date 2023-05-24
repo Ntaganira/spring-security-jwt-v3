bf107472d90bdba79a3ae0d21afb0866383c765a
Ntaganira <heritier.ntaganira@aos.rw>
2023-05-24 18:04:26 +0200

    Addet Nice admin as template

--- src/main/java/rw/heritier/jwt/auth/AuthenticationService.java
+++ src/main/java/rw/heritier/jwt/auth/AuthenticationService.java
@@ -1,10 +1,18 @@
 package rw.heritier.jwt.auth;
 
+import java.io.IOException;
+import java.net.http.HttpRequest;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import rw.heritier.jwt.configuration.JwtService;
 import rw.heritier.jwt.model.Role;
@@ -51,5 +59,33 @@ public class AuthenticationService {
                                 .accessToken(jwtToken)
                                 .refreshToken(jwtRefreshToken)
                                 .build();
+        }
+
+        public void refreshToken(
+                        HttpServletRequest request,
+                        HttpServletResponse response) throws IOException {
+
+                final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+                final String refreshToken;
+                final String userEmail;
+                if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                        return;
+                }
+                // Extraction token from header
+                refreshToken = authHeader.substring(7);
+                // Extracting user email / username
+                userEmail = jwtService.extractUsername(refreshToken);
+                if (userEmail != null) {
+                        var user = this.repository.findUserByEmail(userEmail).orElseThrow();
+                        if (jwtService.isTokenValid(refreshToken, user)) {
+                                var accessToken = jwtService.generateToken(user);
+                                var authResponse = AuthenticationResponseDTO.builder()
+                                                .accessToken(accessToken)
+                                                .refreshToken(refreshToken)
+                                                .build();
+                                new ObjectMapper().writeValue(response.getOutputStream(), authResponse);
+                        }
+                }
         }
 }
