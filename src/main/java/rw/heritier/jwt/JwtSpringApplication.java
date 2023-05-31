6c7f506e658c8389947be177137ecfe92d744f6f
Ntaganira <heritier.ntaganira@aos.rw>
2023-05-31 02:52:34 -0700

    CommandLineRunner was added

--- src/main/java/rw/heritier/jwt/JwtSpringApplication.java
+++ src/main/java/rw/heritier/jwt/JwtSpringApplication.java
@@ -1,7 +1,15 @@
 package rw.heritier.jwt;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import rw.heritier.jwt.auth.AuthenticationService;
+import rw.heritier.jwt.auth.RegisterRequestDTO;
+
+import static rw.heritier.jwt.model.Role.ADMIN;
+import static rw.heritier.jwt.model.Role.MANAGER;
 
 @SpringBootApplication
 public class JwtSpringApplication {
@@ -10,4 +18,19 @@ public class JwtSpringApplication {
 		SpringApplication.run(JwtSpringApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			AuthenticationService service) {
+		return args -> {
+			var admin = RegisterRequestDTO.builder()
+					.firstName("Admin")
+					.lastName("Admin")
+					.email("admin@mail.com")
+					.password("password")
+					.role(ADMIN)
+					.build();
+			// System.out.println("Admin token: " +
+			// service.register(admin).getAccessToken());
+		};
+	}
 }
--- src/main/java/rw/heritier/jwt/auth/RegisterRequestDTO.java
+++ src/main/java/rw/heritier/jwt/auth/RegisterRequestDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import rw.heritier.jwt.model.Role;
 
 @Data
 @Builder
@@ -14,4 +15,5 @@ public class RegisterRequestDTO {
     private String lastName;
     private String email;
     private String password;
+    private Role role;
 }
